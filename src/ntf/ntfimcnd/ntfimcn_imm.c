b2497e28ef679c4d210955b058f710c1c6954220
thuan.tran <thuan.tran@dektech.com.au>
2020-04-27 14:02:39 +0700

    ntf: fix ntfimcn fail to send notification with no space error [#3181]
    - Support NTFA_VARIABLE_DATA_LIMIT configuration for NTF Agent.
    Default value is SHRT_MAX(32767).
    - In system that object creation may have many info attributes/values,
    it should configure this env variable to suitable value for ntfimcn
    able send notification.

--- src/ntf/ntfimcnd/ntfimcn_imm.c
+++ src/ntf/ntfimcnd/ntfimcn_imm.c
@@ -680,8 +680,10 @@ static void saImmOiCcbApplyCallback(SaImmOiHandleT immOiHandle,
 			    ccbUtilOperationData, rdn_attr_name, ccbLast);
 			if (internal_rc != 0) {
 				LOG_ER(
-				    "%s send_object_create_notification fail",
-				    __FUNCTION__);
+				    "%s send_object_create_notification %s fail",
+				    __FUNCTION__,
+				    osaf_extended_name_borrow(
+					    &ccbUtilOperationData->objectName));
 				goto done;
 			}
 			break;
@@ -706,8 +708,10 @@ static void saImmOiCcbApplyCallback(SaImmOiHandleT immOiHandle,
 			    ccbUtilOperationData, invoke_name_ptr, ccbLast);
 			if (internal_rc != 0) {
 				LOG_ER(
-				    "%s send_object_delete_notification fail",
-				    __FUNCTION__);
+				    "%s send_object_delete_notification %s fail",
+				    __FUNCTION__,
+				    osaf_extended_name_borrow(
+					    &ccbUtilOperationData->objectName));
 				goto done;
 			}
 			break;
@@ -720,8 +724,10 @@ static void saImmOiCcbApplyCallback(SaImmOiHandleT immOiHandle,
 			    ccbUtilOperationData, invoke_name_ptr, ccbLast);
 			if (internal_rc != 0) {
 				LOG_ER(
-				    "%s send_object_modify_notification fail",
-				    __FUNCTION__);
+				    "%s send_object_modify_notification %s fail",
+				    __FUNCTION__,
+				    osaf_extended_name_borrow(
+					    &ccbUtilOperationData->objectName));
 				goto done;
 			}
 			break;
