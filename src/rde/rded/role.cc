d1593b03b3c9bec292b14dde65264c261760bf46
Minh Chau <minh.chau@dektech.com.au>
2021-05-28 14:10:57 +1000

    rde: Revert timeout of waiting for peer info [#3263]
    Revert the commit of adding peer info message timeout due to
    disordered of peer up message and peer info message
    
    SC-1 osafrded[190]: NO Got peer info response from node 0x2020f with role Undefined
    SC-1 osafrded[190]: NO Peer up on node 0x2020f
    SC-1 osafimmd[211]: NO MDS event from svc_id 25 (change:3, dest:565217249638969)
    SC-1 osafrded[190]: NO Peer down on node 0x2020f
    
    It sometimes happens because of these messages come from discovery socket
    and data socket at mds

--- src/rde/rded/role.cc
+++ src/rde/rded/role.cc
@@ -196,13 +196,9 @@ Role::Role(NODE_ID own_node_id)
       discover_peer_timeout_{base::GetEnv("RDE_DISCOVER_PEER_TIMEOUT",
                                           kDefaultDiscoverPeerTimeout)},
       pre_active_script_timeout_{base::GetEnv(
-          "RDE_PRE_ACTIVE_SCRIPT_TIMEOUT", kDefaultPreActiveScriptTimeout)},
-      received_peer_info_{true},
-      peer_info_wait_time_{},
-      peer_info_wait_timeout_ {kDefaultWaitPeerInfoTimeout} {}
+          "RDE_PRE_ACTIVE_SCRIPT_TIMEOUT", kDefaultPreActiveScriptTimeout)} {}
 
 timespec* Role::Poll(timespec* ts) {
-  TRACE_ENTER();
   timespec* timeout = nullptr;
   if (role_ == PCS_RDA_UNDEFINED) {
     timespec now = base::ReadMonotonicClock();
@@ -242,25 +238,6 @@ timespec* Role::Poll(timespec* ts) {
         cb->state_refresh_thread_started = true;
         std::thread(&Role::RefreshConsensusState, this, cb).detach();
       }
-      if (consensus_service.IsEnabled() == false) {
-        // We are already ACTIVE, and has just discovered a new node
-        // which makes the election_end_time_ reset
-        if (received_peer_info_ == false) {
-          timespec now = base::ReadMonotonicClock();
-          if (peer_info_wait_time_ >= now) {
-            *ts = peer_info_wait_time_ - now;
-            timeout = ts;
-          } else {
-            // Timeout but haven't received peer info
-            // The peer RDE could be in ACTIVE
-            // thus self-fence to avoid split-brain risk
-            LOG_ER("Discovery peer up without peer info. Risk in split-brain,"
-                "rebooting this node");
-            opensaf_quick_reboot("Probable split-brain due to "
-                "unknown RDE peer info");
-          }
-        }
-      }
     }
   }
   return timeout;
@@ -274,14 +251,9 @@ void Role::ExecutePreActiveScript() {
 }
 
 void Role::AddPeer(NODE_ID node_id) {
-  TRACE_ENTER();
   auto result = known_nodes_.insert(node_id);
   if (result.second) {
     ResetElectionTimer();
-    if (role_ == PCS_RDA_ACTIVE) {
-      ResetPeerInfoWaitTimer();
-      received_peer_info_ = false;
-    }
   }
 }
 
@@ -358,22 +330,8 @@ uint32_t Role::SetRole(PCS_RDA_ROLE new_role) {
 }
 
 void Role::ResetElectionTimer() {
-  TRACE_ENTER();
   election_end_time_ = base::ReadMonotonicClock() +
                        base::MillisToTimespec(discover_peer_timeout_);
-}
-
-void Role::ResetPeerInfoWaitTimer() {
-  TRACE_ENTER();
-  // Reuse peer discovery timeout
-  peer_info_wait_time_ = base::ReadMonotonicClock() +
-                       base::MillisToTimespec(peer_info_wait_timeout_);
-}
-
-void Role::StopPeerInfoWaitTimer() {
-  TRACE_ENTER();
-  // Turn off peer_info_timer
-  received_peer_info_ = true;
 }
 
 uint32_t Role::UpdateMdsRegistration(PCS_RDA_ROLE new_role,
@@ -399,7 +357,6 @@ uint32_t Role::UpdateMdsRegistration(PCS_RDA_ROLE new_role,
 
 void Role::SetPeerState(PCS_RDA_ROLE node_role, NODE_ID node_id,
                         uint64_t peer_promote_pending) {
-  TRACE_ENTER();
   if (role() == PCS_RDA_UNDEFINED) {
     bool give_up = false;
     RDE_CONTROL_BLOCK *cb = rde_get_control_block();
@@ -422,7 +379,6 @@ void Role::SetPeerState(PCS_RDA_ROLE node_role, NODE_ID node_id,
              node_id, to_string(node_role), to_string(role()));
     }
   }
-  StopPeerInfoWaitTimer();
 }
 
 void Role::PromoteNodeLate() {
--- src/rde/rded/role.h
+++ src/rde/rded/role.h
@@ -53,12 +53,9 @@ class Role {
 
  private:
   static const uint64_t kDefaultDiscoverPeerTimeout = 2000;
-  static const uint64_t kDefaultWaitPeerInfoTimeout = 5000;
   static const uint64_t kDefaultPreActiveScriptTimeout = 5000;
   void ExecutePreActiveScript();
   void ResetElectionTimer();
-  void ResetPeerInfoWaitTimer();
-  void StopPeerInfoWaitTimer();
   uint32_t UpdateMdsRegistration(PCS_RDA_ROLE new_role, PCS_RDA_ROLE old_role);
   void PromoteNode(const uint64_t cluster_size, const bool relaxed_mode);
 
@@ -71,9 +68,6 @@ class Role {
   uint64_t pre_active_script_timeout_;
   static const char* const role_names_[];
   static const char* const pre_active_script_;
-  bool received_peer_info_;
-  timespec peer_info_wait_time_;
-  uint64_t peer_info_wait_timeout_;
 
   DELETE_COPY_AND_MOVE_OPERATORS(Role);
 };
